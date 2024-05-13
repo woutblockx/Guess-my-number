60c61a783e13643292eeea78e2b5e43acf9ad692
woutblockx <woutblockxx@gmail.com>
2024-05-13 12:39:45 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = 'Correct Number 🏋️‍♀️';
+
+document.querySelector('.number').textContent = 13;
+
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 5;
